60a1e1f7439af62435a43440fe4d2f51937ead64
binarier <binarier@me.com>
2014-12-26 16:28:25 +0800

    progress

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ mantis-svn
 ==========
 
 使用Mantis用户体系对SVN进行用户和权限方面的集成
+
+requirement:
+
+mod_authnz_external - 集成到httpd用户认证
+
+example:
+
+AddExternalAuth ca /var/lib/mantis-svn/csv-auth.pl
+SetExternalAuthMethod ca pipe
+
+<Location /svn>
+  DAV svn
+  SVNParentPath /opt/svn
+ 
+  AuthType Basic
+  AuthName "xxx Repository"
+
+  AuthBasicProvider external
+  AuthExternal ca
+
+  AuthzSVNAccessFile /opt/svn/svn-access-file
+  Require valid-user
+  ModMimeUsePathInfo on
+  SVNAdvertiseV2Protocol on
+</Location>
--- auth-export.pl
+++ auth-export.pl
@@ -5,6 +5,7 @@ use Digest::MD5 qw(md5 md5_hex md5_base64);
 use File::Basename;
 use lib dirname (__FILE__);
 use config qw(%conf);
+use warnings;
 
 # Connect to target DB
 my $dbh = DBI->connect("DBI:mysql:database=$conf{dbName};host=$conf{dbHost}","$conf{dbUser}","$conf{dbPass}", {'RaiseError' => 1});
@@ -30,8 +31,7 @@ $queryUsers->finish();
 
 # global admin users
 #10:viewer,25:reporter,40:updater,55:developer,70:manager,90:administrator
-my $queryUsers = $dbh->prepare("select id, username from mantis_user_table where enabled=true and access_level>=70");
-my ($id, $username);
+$queryUsers = $dbh->prepare("select id, username from mantis_user_table where enabled=true and access_level>=70");
 $queryUsers->execute();
 $queryUsers->bind_columns(undef, \$id, \$username);
 print $fh "global_admin=";
@@ -48,62 +48,73 @@ $queryUsers->finish();
 my $sqr = $dbh->prepare("select id, name, description from mantis_project_table where enabled=true and status<70");
 
 $sqr->execute();
-my($id, $proj_name, $proj_desc);
+my($proj_name, $proj_desc);
 $sqr->bind_columns(undef, \$id, \$proj_name, \$proj_desc);
 
-my $i = 1;
-my %projects;
+my @projects;
 
 while($sqr->fetch())
 {
+	next if ($proj_desc !~ /svn:(.*)$/m);
+	my $svnurl = $1;
+	my @users;
+	
+	#query for project users
+	#10:viewer,25:reporter,40:updater,55:developer,70:manager,90:administrator
+	$queryUsers = $dbh->prepare("select username from mantis_project_user_list_table pu, mantis_user_table u where pu.user_id = u.id and pu.project_id = ? and pu.access_level >= 55 order by username asc");
+	
+	$queryUsers->execute($id);
+	
+	my $username;
+	$queryUsers->bind_columns(undef, \$username);
+	while ($queryUsers->fetch())
+	{
+		push @users, $username;
+	}
+	$queryUsers->finish();
+	my %pr = (
+		name		=> $proj_name,
+		svnurl	=> $svnurl,
+		users		=> \@users,
+	);
+	push @projects, \%pr;
+}
+$sqr->finish();
+$dbh->disconnect();
 
-next if ($proj_desc !~ /svn:(.*)$/m);
-  %projects{$proj_name} = (
-    svn  => $1,
-    
+#output groups
+foreach my $i (0 .. $#projects)
+{
+	my %project = %{$projects[$i]};
+	print $fh "#Project Name: $project{name}\n";
+	print $fh "proj_$i = ";
+	my $first = 1;
+	foreach my $user (@{$project{users}})
+	{
+		if ($first)
+		{
+			$first = 0;
+		}
+		else
+		{
+			print $fh ",";
+		}
+		
+		print $fh $user;
+	}
+	print $fh "\n\n";
 }
 
-	print $fh "#Project Name: $proj_name\n";
-	print $fh "[$1]\n";
-
+#output permission control
+foreach my $i (0 .. $#projects)
+{
+	my %project = %{$projects[$i]};
+	print $fh "[$project{svnurl}]\n";
 	print $fh "\@global_public = r\n";
 	print $fh "\@global_admin = rw\n";
-
-	#10:viewer,25:reporter,40:updater,55:developer,70:manager,90:administrator
-  $queryUsers = $dbh->prepare("select username from mantis_project_user_list_table pu, mantis_user_table u where pu.user_id = u.id and pu.project_id = ? and pu.access_level >= 55 order by username asc");
-  
-  $queryUsers->execute($id);
-  
-  my $username;
-  
-  $queryUsers->bind_columns(undef, \$username);
-  my $first=1;
-  while ($queryUsers->fetch())
-  {
-  	if (!$first)
-  	{
-  		print $fh ",";
-  	}
-  	$first=0;
-  	print $fh "$username";
-  }
-  
-  if ($first)
-  {
-  	#none user
-#  	print $fh "*=rw\n\n";
-  }
-  else
-  {
-    print $fh "=rw";
-  }
-
-  print $fh "\n\n";
-  
-  $queryUsers->finish();
+	print $fh "\@proj_$i = rw\n";
+	print $fh "\n";
 }
 close $fh;
 
-$sqr->finish();
-$dbh->disconnect();
 
