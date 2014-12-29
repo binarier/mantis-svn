6c64686c13aad61fdd555482287ac8ea76685ec2
binarier <binarier@me.com>
2014-12-29 15:42:22 +0800

    progress

---
+++ auth-dispatch.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use File::Basename;
+use lib dirname (__FILE__);
+use config qw(%conf);
+
+use Net::SCP qw(scp iscp);
+
+$scp = Net::SCP->new("oss-bos", "robot");
+$scp->cwd("/tmp") or die $scp->{errstr};
+$scp->put("$conf{accessFile}") or die $scp->{errstr};
